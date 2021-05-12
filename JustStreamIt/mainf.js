37e1a757345acd93bf2239bf6067ae4ad75fd4db
Arnaud Gagnieu <arnaud.gagnieu@hotmail.fr>
2021-05-12 17:12:29 +0200

    carrousel structure ok, best ok

--- JustStreamIt/mainf.js
+++ JustStreamIt/mainf.js
@@ -2,15 +2,15 @@ let leftBtn = document.getElementsByClassName('left-img');
 let rightBtn = document.getElementsByClassName('right-img');
 let best = document.getElementsByClassName('best-movies-product');
 let horror = document.getElementsByClassName('horror-movies-product');
-let psycho = document.getElementsByClassName('psycho-movies-product');
+let animation = document.getElementsByClassName('animation-movies-product');
 let action = document.getElementsByClassName('action-movies-product');
 let l = 0;
 let m = 0;
 let n = 0;
 let o = 0;
-let movePer = 40;
-let maxMove = 280;
-let minMove = -40;
+let movePer = 25;
+let maxMove = 100;
+let minMove = -25;
 
 let right_mover_best = ()=>{
 	l = l + movePer;
@@ -27,7 +27,7 @@ let left_mover_best = ()=>{
 	if(best == 1){l = 0;}
 	for(const i of best)
 	{
-		if (l <= minMove) {l=240}
+		if (l <= minMove) {l=75}
 		if (l > maxMove){l = l - movePer;}
 		i.style.left = '-' + l + '%';
 	}
@@ -48,28 +48,28 @@ let left_mover_horror = ()=>{
 	if(horror == 1){m = 0;}
 	for(const i of horror)
 	{
-		if (m <= minMove) {m=240}
+		if (m <= minMove) {m=75}
 		if (m > maxMove){m = m - movePer;}
 		i.style.left = '-' + m + '%';
 	}
 }
 
-let right_mover_psycho = ()=>{
+let right_mover_animation = ()=>{
 	n = n + movePer;
-	if (psycho == 1){n = 0;}
-	for(const i of psycho)
+	if (animation == 1){n = 0;}
+	for(const i of animation)
 	{
 		if (n==maxMove) {n=0;}
 		if (n > maxMove){n = n + movePer;}
 		i.style.left = '-' + n + '%';
 	}
 }
-let left_mover_psycho = ()=>{
+let left_mover_animation = ()=>{
 	n = n - movePer;
-	if(psycho == 1){n = 0;}
-	for(const i of psycho)
+	if(animation == 1){n = 0;}
+	for(const i of animation)
 	{
-		if (n <= minMove) {n=240}
+		if (n <= minMove) {n=75}
 		if (n > maxMove){n = n - movePer;}
 		i.style.left = '-' + n + '%';
 	}
@@ -90,7 +90,7 @@ let left_mover_action = ()=>{
 	if(action == 1){o = 0;}
 	for(const i of action)
 	{
-		if (o <= minMove) {o=240}
+		if (o <= minMove) {o=75}
 		if (o > maxMove){o = o - movePer;}
 		i.style.left = '-' + o + '%';
 	}
@@ -99,7 +99,87 @@ rightBtn[0].onclick = ()=>{right_mover_best();}
 leftBtn[0].onclick = ()=>{left_mover_best();}
 rightBtn[1].onclick = ()=>{right_mover_horror();}
 leftBtn[1].onclick = ()=>{left_mover_horror();}
-rightBtn[2].onclick = ()=>{right_mover_psycho();}
-leftBtn[2].onclick = ()=>{left_mover_psycho();}
+rightBtn[2].onclick = ()=>{right_mover_animation();}
+leftBtn[2].onclick = ()=>{left_mover_animation();}
 rightBtn[3].onclick = ()=>{right_mover_action();}
 leftBtn[3].onclick = ()=>{left_mover_action();}
+
+function fetchBest(){
+	let bestTitle = document.getElementsByClassName("best-title");
+	let bestDescription = document.getElementsByClassName("best-text");
+	let bestImg = document.getElementsByClassName("bestFront");
+
+	fetch("http://localhost:8000/api/v1/titles/?imdb_score_min=5&sort_by=-imdb_score&page=1")
+	.then(response => response.json())
+	.then(data => {
+		bestTitle[0].innerHTML = data["results"][0]["title"];
+		bestDescription[0].innerHTML = "Score IMDB du film: " + data["results"][0]["imdb_score"] + "<br>" + "Genres: " + data["results"][0]["genres"];
+		bestImg[0].src = data["results"][0]["image_url"];
+	})
+}
+
+
+fetchBest()
+
+/*function carrouselInfos (){
+	let best = document.getElementsByClassName("best-movies");
+	console.log(best);
+	let title = best[0].getElementsByClassName("under-title");
+	console.log(title);
+	let date = best [0].getElementsByClassName("product-date");
+	let img = best[0].getElementsByClassName("bestFront");
+
+	let i = 0;
+	if (i< 4){
+		fetch("http://localhost:8000/api/v1/titles/?imdb_score_min=5&sort_by=-imdb_score&page=1")
+		.then(response => response.json())
+		.then(data => {
+			for (i=0; i<4; i++){ //Bonne manière de faire ?//
+			
+				title[i].innerHTML = data["results"][i+1]["title"];
+				date[i].innerHTML = data["results"][i+1]["year"];
+			//img[i].src = data["results"][i]["image_url"];//
+			}
+			title[i].innerHTML = data["results"][i+1]["title"];
+			date[i].innerHTML = data["results"][i+1]["year"];
+			//img[i].src = data["results"][i]["image_url"];//
+	i += 4
+}
+
+carrouselInfos()*/
+
+/*function coucou(){
+	let best = document.getElementsByClassName("best-movies");
+	let title = best[0].getElementsByClassName("under-title");
+	let date = best [0].getElementsByClassName("product-date");
+	let img = best[0].getElementsByClassName("best-img");
+	console.log(date[0]);
+
+	for (i=0; i<2; i++){
+		if (i = 0){
+			fetch("http://localhost:8000/api/v1/titles/?imdb_score_min=5&sort_by=-imdb_score&page=1")
+			.then(response => response.json())
+			.then(data => {
+				for (i=0; i<5; i++){
+					console.log(i)
+								
+					console.log(title[i].innerHTML = data["results"][i+1]["title"])
+					date[i].innerHTML = data["results"][i+1]["year"];
+					//img[i].src = data["results"][i]["image_url"];//
+				}
+			})
+		}
+		else {
+			fetch("http://localhost:8000/api/v1/titles/?imdb_score_min=5&sort_by=-imdb_score&page=2")
+			.then(response => response.json())
+			.then(data => {
+				for (i=0; i<3; i++){			
+				title[i+4].innerHTML = data["results"][i]["title"];
+				date[i+4].innerHTML = data["results"][i]["year"];
+				//img[i].src = data["results"][i]["image_url"];//
+				}
+			})
+		}
+	}
+}
+coucou();*/
